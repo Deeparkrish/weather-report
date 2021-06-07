86f88657e344754050dcb0c62897091fb6273802
Deepa <kosalai17@gmail.com>
2021-06-07 16:53:55 -0700

    fixed event listenr bugs

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,53 +2,59 @@ var searchFormEl = document.getElementById("search-form");
 var cityInputEl = document.getElementById("city-name");
 var cityButtonsEl =document.querySelector("#city-buttons");
 var citySelectedEl =document.querySelector("#search-city");
+// var searchButtonEl = document.getElementById("search-btn");
 
-
-// get city name  from input element
-var chosenCityName = cityInputEl.value.trim();
-console.log(cityInputEl);
-// console.log(chosenCityNacime);
-console.log(chosenCityName);
-
-searchFormEl.addEventListener("submit",formSubmitHandler);
+var chosenCityName;
+function toTitleCase(str) {
+    return str.replace(/\w\S*/g, function(txt){
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
+}
 
 var formSubmitHandler = function(event) {
     event.preventDefault();
-    console.log("button was clicked");
-    console.log(event);
+    
+    
+    chosenCityName = cityInputEl.value.trim();
+    console.log(chosenCityName);
+    chosenCityName=toTitleCase(chosenCityName);
+   
     if (chosenCityName) {
-        console.log(chosenCityName);
+        
         displayCityWeather(chosenCityName);
         cityInputEl.value = "";
       } else {
         alert("Please enter a city name");
       }
   };
+  searchFormEl.addEventListener("submit",formSubmitHandler);
 
 
 var displayCityWeather= function(chosenCityName) 
 {
     citySelectedEl.innerHTML =chosenCityName;
-    console.log(chosenCityName);
+    console.log(citySelectedEl);
     
 };
 
-cityButtonsEl.addEventListener("click", buttonClickHandler);
+var getWeatherInfo =function(city){
+    console.log("Weather is good!");
+
+ };
 var buttonClickHandler =function (event){
     event.preventDefault();
     console.log(event);
     event.target.getAttribute("data-cityname");
     if (chosenCityName) {
-        // getWeatherInfo(cityname);
+        getWeatherInfo(chosenCityName);
         console.log(chosenCityName);
         // clear old content
         
       }
 };
+cityButtonsEl.addEventListener("click", buttonClickHandler);
 
-var getWeatherInfo =function (){
-    console.log("function was called");
-   fetch('https://openweathermap.org/find?q=austin');
-    // fetch(https://openweathermap.org/api/one-call-api)
-};
-getWeatherInfo();
+
+ 
+     
+   
