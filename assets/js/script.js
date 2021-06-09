7249d379d9a9f2869b16eb02c2e6a8296b8492b4
Deepa <kosalai17@gmail.com>
2021-06-09 00:16:17 -0700

    changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,7 @@ var windEl = document.querySelector("#wind-city");
 var uvindexEl = document.querySelector("#uvindex-city");
 var MY_API_KEY = "HvaacROi9w5oQCDYHSIk42eiDSIXH3FN";
 
-
+//Convert the first letter of the city name to uppercase
 var chosenCityName;
 function toTitleCase(str) {
     return str.replace(/\w\S*/g, function(txt){
@@ -19,6 +19,27 @@ function toTitleCase(str) {
     });
 }
 
+// get the uvindex  of a city
+var getUVIndex = function (lati, longi){
+    const uvURL = "https://api.openweathermap.org/data/2.5/onecall?lat="+lati+"&lon="+longi+"&exclude=hourly,daily&units=imperial&appid=6acd9728daeb3f35f10da98fa3f7eb4b"
+    fetch(uvURL)
+    .then(function(response) {
+    if(response.ok){
+        response.json().then(function(data) {
+            console.log(data.current.uvi);
+        return (data.current.uvi);
+        
+        });
+    }
+    else {
+        alert('City Not found!'); 
+        
+    } 
+     })  
+    
+}
+
+// Display the city 's current weather details 
 var displayCityWeather= function(chosenCityName, data) 
 {
     if (data.length === 0) {
@@ -27,23 +48,23 @@ var displayCityWeather= function(chosenCityName, data)
     }
     var thisDate = moment().format('MM/DD/YY');
     var chosenCityTitle=toTitleCase(chosenCityName);
+
     citySelectedEl.innerHTML =chosenCityTitle+" "+thisDate;
-    tempEl.innerHTML ="Temperature  :"+data.main.temp;
-    humidityEl.innerHTML ="Humidity :"+data.main.humidity;
-    windEl.innerHTML ="Wind :"+ data.wind.speed;
+    tempEl.innerHTML ="Temperature  :"+data.main.temp+"°F";
+    humidityEl.innerHTML ="Humidity :"+data.main.humidity +"%";
+    windEl.innerHTML ="Wind :"+ data.wind.speed +"MPH";
     
     var cityLat = data.coord.lat;
     var cityLon = data.coord.lon;
-    var UVIndexEl =getUVIndex(cityLat,cityLon);
-    
-
-
-    // responseContainerEl.innerHTML = `<img src=${response.data.image_url}  />`;
+    var UVIndexEl = getUVIndex(cityLat,cityLon);
+    console.log(UVIndexEl);
+    // if (UVIndexEl>=0 && UVIndexEl <=2) {uvindexEl.classList.add("bg-success", "text-white");}
+    // else if(UVIndexEl >=3 && UVIndexEl<=5){uvindexEl.classList.add("bg-danger","bg-warning", "text-white");}
+    // else if(UVIndexEl ==6 || UVIndexEl==7){uvindexEl.classList.add("bg-warning", "text-white");}
+    // else if (UVIndexEl >=8 && UVIndexEl<=10){uvindexEl.classList.add("bg-danger", "text-white");}
+    // else {uvindexEl.classList.add("bg-danger", "bg-primary", "text-white");}
 };
-var getUVIndex = function (){
 
-
-}
 var getWeatherInfo =function(city){
     console.log("Weather is good!");
     const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&units=imperial&appid=6acd9728daeb3f35f10da98fa3f7eb4b"
@@ -59,10 +80,9 @@ var getWeatherInfo =function(city){
         alert('City Not found!');
             }  
     });
-//     .catch(function(error) {
-//         // Notice this `.catch()` getting chained onto the end of the `.then()` method
-//         alert("Unable to connect to Weathermap");
-//       });
+    //  .catch(function(error) {
+    //    alert("Unable to connect to Weathermap");
+    // });
 }
 
 var formSubmitHandler = function(event) {
@@ -73,7 +93,7 @@ var formSubmitHandler = function(event) {
         getWeatherInfo(chosenCityName);
         cityInputEl.value = "";
        
-    var citLon = data.coord.lon;
+    
       } else {
         alert("Please enter a city name");
       }
